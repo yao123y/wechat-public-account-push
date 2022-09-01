c2e5b8b147b02daaef527dac13786dbc1a7e235e
yao123y <112600039+yao123y@users.noreply.github.com>
2022-09-01 13:05:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,98 +1,27 @@
-export const config = {
-    /**
-     * 公众号配置
-     */
-
-    // 公众号APP_ID
-    APP_ID: "",
-
-    // 公众号APP_SECRET
-    APP_SECRET: "",
-
-    // 模板消息id
-    TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
-
-    // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
-
-    // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-    // [{
-    //   name: "张三",
-    //   id: "wxafasdasdd-zxfvsdfd"
-    // },
-    // {
-    //   name: "李四",
-    //   id: "wxafasdasdd-zxfvsdfd"
-    // }]
-    USERS: [
-      {name: '老婆0', id: "1hxOO6VskYv0Kuf-y你懂的,填错狗头打烂VRMpuXhk"}, 
-      {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
-      {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
-      {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
-    ],
-    // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-    CALLBACK_USERS: [
-      {name: '自己',id: "5hxOO6VskYv你懂的,填错狗头打烂-ywIZVRMpuXhk"}, 
-    ],
-     
-    /**
-     * 信息配置
-     */
-
-    /** 天气相关 */
-
-    // 所在省份
-    PROVINCE: "广东",
-    // 所在城市
-    CITY: "惠州",
-
-    /** 重要节日相关 */
-
-    // 重要节日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
-    // tpye必须填！ 只能 “生日” 和 “节日” 二选一!
-    // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
-    // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
-    FESTIVALS: [
-      {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
-      {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
-      {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
-      {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
-    ],
-    // 限制重要节日的展示条目, 需要填写数字; 
-    // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
-    // 如果为0, 则默认展示全部
-    FESTIVALS_LIMIT: 3,
-
-    /** 日期相关 */
-
-    // 在一起的日子，格式同上
-    LOVE_DATE: "2015-05-01",
-    // 结婚纪念日
-    MARRY_DATE: "2020-01-04",
-
-
-    /** 每日一言 */
-
-    // 好文节选的内容类型
-    // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-    LITERARY_PREFERENCE: ""
-
-
-    }
-
-// {{date.DATA}}  
-// 城市：{{city.DATA}}  
-// 天气：{{weather.DATA}}  
-// 最低气温: {{min_temperature.DATA}}  
-// 最高气温: {{max_temperature.DATA}}  
-// 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
-// {{birthday_message.DATA}}
-
-// {{note_en.DATA}}  
-// {{note_ch.DATA}}
-
-// 共推送 {{need_post_num.DATA}}  人
-// 成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
-// 成功用户: {{success_post_ids.DATA}}
-// 失败用户: {{fail_post_ids.DATA}}
+{
+# 公众号配置
+# 公众号appId
+"app_id": "wx12b1881daf068231",
+# 公众号appSecret
+"app_secret": "fe9380f1b54df43cc9bfda46c6186655",
+# 模板消息id
+"template_id": 4hNu0IIETzk6o3i9u7mcR69HJgViKzsCLz1IF9IenqY",
+# 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
+"user": ["oHviZ5ioAHOODqOhQL1J6PqmfgCY，oHviZ5iVWP5_c5xytPbsVx-ltZJk"],
+ 
+# 信息配置
+# 和风天气apikey
+"weather_key": "f7ecb413879e4f7d9577b24de7b59557",
+# 所在地区，可为省，城市，区，县，同时支持国外城市，例如伦敦
+"region": "厦门市翔安区",
+# 生日1，修改名字为对应需要显示的名字，如果生日为农历，在最前面加上r即可
+"birthday1": {"name": "小鱼", "birthday": "2004-09-18"},
+# 生日2
+"birthday2": {"name": "小羊", "birthday": "2004-03-27"},
+# 在一起的日子，格式同上，暂不不支持农历
+"love_date": "2022-08-06",
+# 金句中文，如果设置了，则会显示这里的，如果为空，默认会读取金山的每日金句
+"note_ch": "",
+# 金句英文
+"note_en": ""
+}
